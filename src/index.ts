cf6998548e86452586559b505ab1226fb5d10da8
Erlend <erlend.eelmets@gmail.com>
2024-07-30 15:05:12 +0300

    Basic error handling

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,37 @@
-import express, { type Express, type Request, type Response } from "express";
+import express, {
+  type Express,
+  type Request,
+  type Response,
+  type NextFunction,
+} from "express";
 
 import User from "./models/user";
 
-const PORT = 3000;
+const PORT: number = 3000;
 const app: Express = express();
 
-app.get('/api/v1/create-test-user', async (request: Request, response: Response) => {
-    const newUser = new User({username: "test"})
-    await newUser.save()
-    response.status(201).json(newUser)
-})
+app.get(
+  "/api/v1/create-test-user",
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const newUser = new User({ username: "test" });
+      await newUser.save();
+      response.status(201).json(newUser);
+    } catch (error) {
+      response.status(500).send("Internal Server Error");
+    }
+  }
+);
 
-app.get('/api/v1/users', async (request: Request, response: Response) => {
-    const users = await User.find({})
-    response.json(users)
-})
+app.get("/api/v1/users", async (request: Request, response: Response) => {
+  try {
+    const users = await User.find({});
+    response.status(200).json(users);
+  } catch (error) {
+    response.status(500).send("Internal Server Error");
+  }
+});
 
-
-app.listen(PORT, ()=> {
-    console.log(`Server running at http://127.0.0.1:${PORT}/api/v1/users`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running at http://127.0.0.1:${PORT}/api/v1/users`);
+});
